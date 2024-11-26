d4c0d51da1b4146596ed8a572f2fe766c668c34a
amirh <amirh9476@gmail.com>
2024-11-26 10:29:12 +0330

    Add models.py videos

---
+++ netflix/videos/models/video.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils import timezone
+
+
+class VideoQuerySet(models.QuerySet):
+    def published(self):
+        now = timezone.now()
+        return self.filter(
+            state=PublishStateOptions.PUBLISH,
+            publish_timestamp_lte = now
+        )
+
+class VideoManager(models.Manager):
+    def get_queryset(self):
+        return VideoQuerySet(self.model, using=self._db)
+    def published(self):
+        return self.get_queryset().published()
+
+class Video(models.Model):
+    title = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True) # 'this_is_my_video'
+    video_id = models.CharField(max_length=220, unique=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    state = models.CharField(max_length=2, choices=PublishStateStateOptions.choices, default=PublishStateOptions.DRAFT)
+    publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
+
+    objects = VideoManager()
+
+    def get_video_id(self):
+        if not self.is_published:
+            return None
+        return self.video_id
+
+    @property
+    def is_published(self):
+        if self.active is False:
+            return False
+        state = self.state
+        if state != PublishStateOptions.PUBLISH:
+            return False
+        pub_timestamp = self.publish_timestamp
+        if pub_timestamp is None:
+            return False
+        now = timezone.now()
+        return pub_timestamp <= now
+
---
+++ netflix/videos/models/video_proxy.py
@@ -0,0 +1,29 @@
+from django.db.models.signals import pre_save
+
+from videos.models.video import Video
+
+
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'All Video'
+        verbose_name_plural = 'All Videos'
+
+
+class VideoPublishedProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = 'Published Videos'
+
+
+pre_save.connect(publish_state_pre_save, sender=Video)
+pre_save.connect(slugify_pre_save, sender=Video)
+
+
+pre_save.connect(publish_state_pre_save, sender=VideoAllProxy)
+pre_save.connect(slugify_pre_save, sender=VideoAllProxy)
+
+
+pre_save.connect(publish_state_pre_save, sender=VideoPublishedProxy)
+pre_save.connect(slugify_pre_save, sender=VideoPublishedProxy)
